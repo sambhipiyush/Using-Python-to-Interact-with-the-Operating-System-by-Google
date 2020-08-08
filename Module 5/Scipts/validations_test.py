bb4ee0d9111e5472c666d40b8907175181b320c1
sambhipiyush <piyush.sambhi07@icloud.com>
2020-08-09 04:16:27 +0530

    Added test function in validations_test.py

--- Module 5/Scipts/validations_test.py
+++ Module 5/Scipts/validations_test.py
@@ -14,6 +14,8 @@ class TestValidateUser(unittest.TestCase):
 	def test_invalid_characters(self):
 		self.assertEqual(validate_user('invalid_user', 1), False)
 
+	def test_invalid_minlen(self):
+		self.assertRaises(ValueError, validate_user, "user", -1)
 
 # Run the test
 unittest.main()
